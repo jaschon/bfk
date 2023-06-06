63dbaf394ec5077e00378e0b92856a7ca3e2a3db
Jason Rebuck <jaschon@users.noreply.github.com>
2023-06-06 10:45:15 -0700

    Update bf.py
    Fix input to only take one character. Wrap +/- at 127.

--- bf.py
+++ bf.py
@@ -1,11 +1,14 @@
 #!/usr/bin/env python3
-#Jaschon 2023
+"""brainf*ck programming language interpreter"""
 
+import sys
+
+__author__ = "jaschon"
+__copyright__ = "2023"
 
 class BF_K:
-
     def __init__(self, pgm):
-        self.pgm = pgm 
+        self.pgm = pgm
         self.ptr = 0
         self.counter = 0
         self.stack = []
@@ -13,11 +16,11 @@ class BF_K:
 
     def _plus(self):
         """opt code for + char"""
-        self.cells[self.ptr] = 0 if self.cells[self.ptr] == 255 else self.cells[self.ptr] + 1
+        self.cells[self.ptr] = 0 if self.cells[self.ptr] == 127 else self.cells[self.ptr] + 1
 
     def _minus(self):
         """opt code for - char"""
-        self.cells[self.ptr] = 255 if self.cells[self.ptr] == 0 else self.cells[self.ptr] - 1
+        self.cells[self.ptr] = 127 if self.cells[self.ptr] == 0 else self.cells[self.ptr] - 1
 
     def _cell_r(self):
         """opt code for > char"""
@@ -57,7 +60,7 @@ class BF_K:
 
     def _in(self):
         """opt code for , char"""
-        self.cells[self.ptr] = ord(input()[0])
+        self.cells[self.ptr] = ord(sys.stdin.read(1))
 
     def run(self):
         """run each character opt code"""
